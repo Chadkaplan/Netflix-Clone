6ef5961903fd0d190ca59bf16b58f63933725d98
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-02-01 18:12:11 -0500

    Switched to material design icons.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./navbar.css";
 import logo from "./netflixlogo.jpg";
-import { GiMagnifyingGlass } from "react-icons/gi";
-import { BsBell } from "react-icons/bs";
-import { CgProfile } from "react-icons/cg";
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 export default function Navbar() {
   return (
@@ -18,14 +18,14 @@ export default function Navbar() {
       </ul>
       <div className="navbar--group navbar--group-right">
         <h6 className="nav--item">
-          <GiMagnifyingGlass />
+          <SearchOutlinedIcon />
         </h6>
-        <h6 className="nav--item">Kids</h6>
+        <h5 className="nav--item pad-top">Kids</h5>
         <h6 className="nav--item">
-          <BsBell />
+          <NotificationsIcon />
         </h6>
         <h6 className="nav--item">
-          <CgProfile />
+          <AccountBoxIcon />
         </h6>
       </div>
     </header>
