f420ac5c57c9a55caf41b4f7e818a6d7ffd63af2
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-02-06 14:21:20 -0500

    Added horizontal scrolling with scroll bar within each genre to eliminate overflow off the screen.

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./movie.css";
 
 export default function Movie(props) {
-  console.log(props)
   return (
     <div>
       <img className="movie--icon" src={props.img} alt={props.title} />
