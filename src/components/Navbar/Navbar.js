1b4d32b8ac40b813cbda800d3675643b848c62d9
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-02-07 18:34:56 -0500

    Fixed alignment issue and reduced class names.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -10,21 +10,21 @@ export default function Navbar() {
     <header className="Navbar">
       <ul className="Navbar__group">
         <img className="Navbar__netflixlogo" src={logo} alt="netflix" />
-        <li className="Navbar__item">Home</li>
-        <li className="Navbar__item">TV Shows</li>
-        <li className="Navbar__item">Movies</li>
-        <li className="Navbar__item">New &amp; Popular</li>
-        <li className="Navbar__item">My List</li>
+        <li>Home</li>
+        <li>TV Shows</li>
+        <li>Movies</li>
+        <li>New &amp; Popular</li>
+        <li>My List</li>
       </ul>
       <div className="Navbar__group Navbar__group--right">
-        <h6 className="Navbar__item">
+        <h6>
           <SearchOutlinedIcon />
         </h6>
-        <h5 className="Navbar__item Navbar__item--pad">Kids</h5>
-        <h6 className="Navbar__item">
+        <h5 className="Navbar__item">Kids</h5>
+        <h6>
           <NotificationsIcon />
         </h6>
-        <h6 className="Navbar__item">
+        <h6>
           <AccountBoxIcon />
         </h6>
       </div>
