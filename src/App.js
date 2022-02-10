01ede19b2c06113c2eeff6d187cf57659d121f4b
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-02-09 23:47:53 -0500

    Setup dynamic URL Querying to API.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import Main from "./components/Main/Main";
 import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_API_KEY
+let API_URL = "Crazy+stupid+love"
 
 function App() {
-  axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&t=Crazy+stupid+love`)
+  axios.get(`http://www.omdbapi.com/?apikey=${API_KEY}&t=${API_URL}`)
   .then(res => {
     const movie = res.data;
     console.log(movie);
-  })
+  }).catch(function (error) {
+    if (error.response) {
+      console.log(error.response.data);
+  }})
   return (
     <div className="App">
       <Navbar />
