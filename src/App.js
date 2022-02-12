870a97fe44402439d0dedbf0eb5b7c5158610100
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-02-11 19:54:39 -0500

    Promise.all() returning empty values.

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ let API_URL = "the+a";
 let searchParam = "s";
 
 function App() {
-  const [moviesList, setMoviesList] = useState([]);
-  const [moviesToQuery, setMoviesToQuery] = useState([]);``
+  const [moviesList, setMoviesList] = useState([{Poster: "https://m.media-amazon.com/images/M/MV5BZThjMmQ5YjktMTUyMC00MjljLWJmMTAtOWIzNDIzY2VhNzQ0XkEyXkFqcGdeQXVyMTAyNjg4NjE0._V1_SX300.jpg",
+  Title: "The Perks of Being a Wallflower",
+  Type: "movie",
+  Year: "2012",
+  imdbID: "tt1659337"}]);
+  const [moviesToQuery, setMoviesToQuery] = useState([]);
   useEffect(() => {
     fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${API_URL}`)
       .then((res) => res.json())
@@ -18,20 +22,19 @@ function App() {
         console.log("Requestfailed", error);
       });
   }, []);
-
+    
   useEffect(() => {
     Promise.all(
-      moviesToQuery.forEach((movie) => {
+      moviesToQuery.map((movie) => {
         fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&t=${movie.imdbID}`);
       })
     )
-      .then((values) => {
-        console.log(values);
-      })
+      .then((values) => console.log("Values: ", values))
       .catch(function (error) {
         console.log("Requestfailed", error);
       });
   }, []);
+  // console.log("To Query: ",moviesToQuery)
   return (
     <div className="App">
       <Navbar />
