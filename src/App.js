6095d7aa63f4dac6a18971147948b2d61bf2ae59
Chad Kaplan <chadkaplan@knights.ucf.edu>
2022-03-01 10:27:12 -0500

    Unexpected error of map() method reading properties of undefined during async/await transition.

--- src/App.js
+++ src/App.js
@@ -8,9 +8,8 @@ let API_URL = "the+a";
 function App() {
   const [moviesList, setMoviesList] = useState([]);
 
-  const asyncAPI = () => {
-    try {
-      let fetchAPI = async (pages) => {
+
+  let fetchAPI = async (pages) => {
         // 10 movies per query to get ID's
         let paginatedResponse = await fetch(
           `http://www.omdbapi.com/?apikey=${API_KEY}&s=${API_URL}&page=${pages}`
@@ -19,13 +18,13 @@ function App() {
         let moviesToQuery = paginatedResponse.Search;
         let eachMovie;
         // Get each movie's full object individually
-        let individualMovies = await Promise.all(
-          (eachMovie = await moviesToQuery.map((movie) => {
-            fetch(
+        let individualMovies = Promise.all(
+           moviesToQuery.map( async (movie) => {
+           return eachMovie = await fetch(
               `http://www.omdbapi.com/?apikey=${API_KEY}&i=${movie.imdbID}`
             );
-            eachMovie = eachMovie.json();
-          }))
+            eachMovie = eachMovie.json()
+          })
         );
         // Update the state to add each individual movie
         return (individualMovies = setMoviesList((currentValues) => [
@@ -33,16 +32,12 @@ function App() {
           ...values,
         ]));
       };
+      // Iterate over this many pages of 10
       for (let pages = 0; pages < 4; pages++) {
         fetchAPI(pages);
       }
-    } catch (error) {
-      console.log(error);
-    }
-    // Iterate over this many pages of 10
-  };
   useEffect(() => {
-    asyncAPI();
+    fetchAPI();
   }, []);
 
   console.log("Render App");
